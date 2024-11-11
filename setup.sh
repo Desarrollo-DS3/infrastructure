b262d8efdc91e0166c8ee839a783585932c547f0
Nicolas Herrera <herreran903@gmail.com>
2024-11-10 22:56:29 -0500

    Stock deployment

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 # Crear el clúster K3D
-k3d cluster create store-cluster -p "8000:30000@agent:1" -p "8001:30100@agent:0" --port 50840:80@loadbalancer --agents 4
+k3d cluster create store-cluster -p "8000:30100@agent:0" --port 50840:80@loadbalancer --agents 4
 
 # Taint y label de nodos
 kubectl taint nodes k3d-store-cluster-server-0 dedicated=server:NoSchedule
-kubectl label nodes k3d-store-cluster-agent-0 gateway=true
-kubectl label nodes k3d-store-cluster-agent-1 test=true
 kubectl label nodes k3d-store-cluster-agent-2 stock=true
-kubectl label nodes k3d-store-cluster-agent-3 transaction=true
 
 # Aplicar el operador de RabbitMQ
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
@@ -46,16 +43,6 @@ kubectl port-forward -n rabbitmq-system rabbit-server-0 8080:15672 &
 kubectl apply -f ./stock/stock-config.yaml
 kubectl apply -f ./stock/stock-secrets.yaml
 kubectl apply -f ./stock/stock-deployment.yaml
-
-kubectl apply -f ./transaction/transaction-config.yaml
-kubectl apply -f ./transaction/transaction-secrets.yaml
-kubectl apply -f ./transaction/transaction-db-volume.yaml
-kubectl apply -f ./transaction/transaction-db-deployment.yaml
-kubectl apply -f ./transaction/transaction-deployment.yaml
-
-kubectl apply -f ./test-microservice/test-deployment.yaml
-
-kubectl apply -f ./gateway/gateway-deployment.yaml
 
 # Obtener todos los recursos en el namespace rabbitmq-system
 # kubectl get all -l app.kubernetes.io/name=rabbit -n rabbitmq-system
